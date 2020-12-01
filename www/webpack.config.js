31c4f0723a8d8f86cf658570f99d3ae086d29369
ts-87 <taka87874k4@gmail.com>
2020-12-02 01:44:49 +0900

    bug fix

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,13 +2,15 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: {
+    gol: "./gol.js"
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "[name].js",
   },
   mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+  //plugins: [
+  //  new CopyWebpackPlugin(['index.html'])
+  //],
 };
